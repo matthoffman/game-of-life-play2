005ea01c83302273689a30d9c0e1a08a4b330e37
mhoffman <matt.hoffman@quantumretail.com>
2012-04-23 00:53:33 -0400

    Adding evolution logic, along with some tests

--- src/main/java/org/mhoffman/life/LifeBoard.java
+++ src/main/java/org/mhoffman/life/LifeBoard.java
@@ -2,10 +2,16 @@ package org.mhoffman.life;
 
 /**
  * Represents a board for the game of life.
- * This object is immutable; it will return a
+ * This object is immutable; it will return a new instance of LifeBoard on each call to {@link #evolve()}.
  * <p/>
  * Eventually, if we find ourselves wanting multiple implementations (perhaps different rule variations?) we will want
  * to change this to an interface. But that's not necessary yet.
+ * <p/>
+ * Note that this treats cells as ints, instead of booleans. Since cells are either alive or dead, I certainly could
+ * have treated them as booleans. But I decided to go int instead, simply because there are some interesting variations
+ * of Conway's original rules that involve multiple colors. This is probably a case where I should stick to YAGNI and
+ * use booleans, but it was just as easy. If I need to optimize for very, very large boards later, I can switch back to
+ * booleans.
  */
 public class LifeBoard {
     // normally, i'm against arrays instead of ArrayLists; after all, ArrayList gives you expansion for free...
@@ -19,7 +25,43 @@ public class LifeBoard {
 
 
     public LifeBoard evolve() {
-        return null;  //To change body of created methods use File | Settings | File Templates.
+        // for each cell,
+        //    extract its neighbors
+
+        return new LifeBoard(new int[1][1]);//TODO: implement me
+    }
+
+    /**
+     * Return the new state of the this cell, given the state of its neighbors
+     * <p/>
+     * The code should take this board and create a new board for the next
+     * generation based on the following rules:
+     * 1) Any live cell with fewer than two live neighbours dies (underpopulation)
+     * 2) Any live cell with two or three live neighbours lives on to the
+     * next generation (survival)
+     * 3) Any live cell with more than three live neighbours dies
+     * (overcrowding)
+     * 4) Any dead cell with exactly three live neighbours becomes a live
+     * cell (reproduction)
+     *
+     * @return
+     */
+    public int evolveCell(int cell, int[] neighbors) {
+        // note that we're not checking how many neighbors we have. That way, we can choose to call this for cells along
+        // the edges, if we decide to treat the edges as automatically "dead".
+        int numberOfLiveNeighbors = countLiveCells(neighbors);
+        if (numberOfLiveNeighbors > 3) return 0; // died of overcrowding
+        if (cell == 0 && numberOfLiveNeighbors == 3) return 1; // reproduction!
+        if (cell == 1 && numberOfLiveNeighbors < 2) return 0; // underpopulation
+        return cell;// stay as it is.
+    }
+
+    private int countLiveCells(int[] neighbors) {
+        int sum = 0;
+        for (int neighbor : neighbors) {
+            if (neighbor == 1) sum++;
+        }
+        return sum;
     }
 
     /**
---
+++ src/test/scala/org/mhoffman/life/LifeBoardTest.scala
@@ -0,0 +1,133 @@
+package org.mhoffman.life
+
+import org.scalatest.FlatSpec
+import org.scalatest.matchers.ShouldMatchers
+
+
+/**
+ *
+ *
+ */
+
+class LifeBoardTest extends FlatSpec with ShouldMatchers {
+  //////////////////////////
+  // living cell tests:
+  //////////////////////////
+  "a living cell" should "die if it has only 1 live neighbor" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 0, 0, 0, 0, 0, 0, 0))
+    result should equal(0)
+  }
+
+  "a living cell" should "die if it has no live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(0, 0, 0, 0, 0, 0, 0, 0))
+    result should equal(0)
+  }
+
+  "a living cell" should "survive if it has 2 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 1, 0, 0, 0, 0, 0, 0))
+    result should equal(1)
+  }
+
+  "a living cell" should "survive if it has 2 live neighbors -- no matter which neighbors they are" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(0, 0, 1, 0, 0, 0, 0, 1))
+    result should equal(1)
+  }
+
+  "a living cell" should "survive if it has 3 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(0, 1, 0, 1, 0, 0, 1, 0))
+    result should equal(1)
+  }
+
+  "a living cell" should "die if it has 4 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 0, 1, 0, 1, 0, 0, 1))
+    result should equal(0)
+  }
+
+  "a living cell" should "die if it has 5 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 0, 1, 1, 1, 0, 0, 1))
+    result should equal(0)
+  }
+
+  "a living cell" should "die if it has 6 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 0, 1, 1, 1, 0, 1, 1))
+    result should equal(0)
+  }
+
+  //////////////////////////
+  // dead cell tests:
+  //////////////////////////
+  "a dead cell" should "stay dead if it has only 1 live neighbor" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(1, 0, 0, 0, 0, 0, 0, 0))
+    result should equal(0)
+  }
+
+  "a dead cell" should "stay dead if it has no live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(0, 0, 0, 0, 0, 0, 0, 0))
+    result should equal(0)
+  }
+
+  "a dead cell" should "stay dead if it has 2 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(1, 1, 0, 0, 0, 0, 0, 0))
+    result should equal(0)
+  }
+
+  "a dead cell" should "stay dead if it has 2 live neighbors -- no matter which neighbors they are" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(0, 0, 1, 0, 0, 0, 0, 1))
+    result should equal(0)
+  }
+
+  "a dead cell" should "come to life if it has 3 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(0, 1, 0, 1, 0, 0, 1, 0))
+    result should equal(1)
+  }
+
+  "a dead cell" should "come to life if it has 3 live neighbors -- no matter which neighbors they are" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 1, 0, 0, 0, 0, 1, 0))
+    result should equal(1)
+  }
+
+  "a dead cell" should "stay dead if it has 4 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(1, 0, 1, 0, 1, 0, 0, 1))
+    result should equal(0)
+  }
+
+  "a dead cell" should "stay dead if it has 5 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(1, 0, 1, 1, 1, 0, 0, 1))
+    result should equal(0)
+  }
+
+  "a dead cell" should "stay dead if it has 6 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(0, Array(1, 0, 1, 1, 1, 0, 1, 1))
+    result should equal(0)
+  }
+
+  // odd data tests
+  "a living cell" should "stay alive with ONLY 3 live neighbors" in {
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array(1, 1, 1))
+    result should equal(1)
+  }
+  "a living cell" should "die with no neighbors at all" in {
+    // i don't know how this could possibly happen, but might as well handle it .
+    val lifeBoard = new LifeBoard(Array(Array(0, 0, 0), Array(0, 0, 0), Array(0, 0, 0))) // note that, for this method, we don't care about the passed-in array
+    val result = lifeBoard.evolveCell(1, Array())
+    result should equal(0)
+  }
+}
